49ccba017d6f1bf90cfb97eb5541f78bc5eeadc7
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-03-12 16:42:20 +0330

    finished

--- transaction/views.py
+++ transaction/views.py
@@ -3,8 +3,9 @@ from django.http.response import HttpResponse
 # Create your views here.
 
 BOODJE = 100000
-def my_account(request,username):
-    return render(request,"./templates/transaction/main.html")
+
+def my_account(request, username):
+    return render(request, "transaction/main.html", context={'username': username})
 
 def get_loan(request,darkhast):
     global BOODJE
